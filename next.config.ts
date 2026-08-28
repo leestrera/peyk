351170e24b8f72095b5a7da5259403f481f6fe42
Lorenz Edward T. Estrera <leestrera647.pbox@parsu.edu.ph>
2026-08-28 19:48:27 +0800

    chore(config): add new local ip to allowed dev origins

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.1.5', '192.168.1.2', '10.226.47.1'],
+  allowedDevOrigins: ['192.168.1.5', '192.168.1.2', '10.226.47.1', '192.168.1.10'],
   images: {
     remotePatterns: [
       {
